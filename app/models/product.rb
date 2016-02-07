2718e0cca0baf25515080293c907448f5176fa1f
webexninja <shivanktiware@gmail.com>
2016-02-07 23:14:50 +0530

    Fix bug in product.rb and seed the db

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ActiveRecord::Base
 	validates :name, presence: true
-	validates :price, presence: true, numaricality: true, format: { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }
+	validates :price, presence: true, numericality: true, format: { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }
 end
