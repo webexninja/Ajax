495dd09a82bb677a81cb22003113907dc89c5409
webexninja <shivanktiware@gmail.com>
2016-02-07 23:12:36 +0530

    add validations and seed data

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ActiveRecord::Base
+	validates :name, presence: true
+	validates :price, presence: true, numaricality: true, format: { :with => /\A\d{1,4}(\.\d{0,2})?\z/ }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Product.delete_all
+
+Product.create!([
+	{id: 1, name: "Nintendo Wii U Premium", price: 250},
+	{id: 2, name: "XBox 360 250GB", price: 250},
+	{id: 3, name: "Playstation 3 500 GB", price: 239.95},
+	{id: 4, name: "Nintendo Wii", price: 99.95},
+	{id: 5, name: "Nintendo 3DS", price: 174.95}
+])
